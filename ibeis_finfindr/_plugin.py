8127501821652d4043a2c6c42246fa3504210c27
Jason Parham <parham@wildme.org>
2019-11-11 20:13:12 -0800

    Updated chunksize

--- ibeis_finfindr/_plugin.py
+++ ibeis_finfindr/_plugin.py
@@ -390,7 +390,7 @@ class FinfindrDistanceConfig(dt.Config):  # NOQA
     colnames=['distance'], coltypes=[float],
     configclass=FinfindrDistanceConfig,
     fname='finfindr',
-    chunksize=1000)
+    chunksize=10000)
 def finfindr_distance_depc(depc, qaid_list, daid_list, config):
     # qaid and aid lists are parallel
     # The doctest for ibeis_plugin_deepsense_identify_deepsense_ids also covers this func
